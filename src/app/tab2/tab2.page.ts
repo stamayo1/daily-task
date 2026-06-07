50ede8fd09b64ce5a06922d5c2fa6577c0b05191
sTamayo <sebastian18t@gmail.com>
2026-06-07 01:41:26 -0500

    refactor: replace inline ion-headers with shared header component across all tabs

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonContent, ExploreContainerComponent, HeaderComponent]
 })
 export class Tab2Page {
-
   constructor() {}
-
 }

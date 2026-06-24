2306bb87fb8f2633e188a270a79e7b9bfff6e37e
sTamayo <sebastian18t@gmail.com>
2026-06-23 21:50:54 -0500

    refactor: migrate user repository

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,11 @@ import { inject, provideAppInitializer } from '@angular/core';
 import { AppInitializer } from './app/core/services/appInitializer/app-initializer';
 import { TaskRepository } from './app/core/domain/repositories/task.repository';
 import { CategoryRepository } from './app/core/domain/repositories/category.repository';
+import { UserRepository } from './app/core/domain/repositories/user.repository';
 import { AnalyticsPort } from './app/core/domain/repositories/analytics.port';
 import { SqliteTaskRepository } from './app/core/infrastructure/persistence/sqlite/sqlite-task.repository';
 import { SqliteCategoryRepository } from './app/core/infrastructure/persistence/sqlite/sqlite-category.repository';
+import { SqliteUserRepository } from './app/core/infrastructure/persistence/sqlite/sqlite-user.repository';
 import { FirebaseAnalyticsAdapter } from './app/core/infrastructure/analytics/firebase-analytics.adapter';
 
 const initializeFactory = () => async () => {
@@ -27,6 +29,7 @@ bootstrapApplication(AppComponent, {
     AppInitializer,
     { provide: TaskRepository, useClass: SqliteTaskRepository },
     { provide: CategoryRepository, useClass: SqliteCategoryRepository },
+    { provide: UserRepository, useClass: SqliteUserRepository },
     { provide: AnalyticsPort, useClass: FirebaseAnalyticsAdapter },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
